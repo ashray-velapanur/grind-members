15c3d01cd1287da6452bf5a472359784bd4f240a
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-12-07 18:11:59 +0530

    Adding page to enter cobot space(room) ID and image

--- grind-code/application/controllers/admin/locationmanagement.php
+++ grind-code/application/controllers/admin/locationmanagement.php
@@ -183,6 +183,29 @@ class LocationManagement extends CI_Controller {
 				
 		redirect(g_url("/admin/index/dashboard"));
 	}
+
+	public function cobot_space() {
+		$this->load->view("/admin/add_space.php");
+	}
+
+	public function add_update_space() {
+		error_log("In add_update_space");
+		$target_dir = "uploads/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+			if($check !== false) {
+				error_log("File is an image - " . $check["mime"] . ".");
+				$uploadOk = 1;
+			} else {
+				error_log("File is not an image.");
+				$uploadOk = 0;
+			}
+		}
+	}
 }
 
 ?>
---
+++ grind-code/application/views/admin/add_space.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="/~aiyappaganesh/grind-members/grind-code/index.php/admin/locationmanagement/add_update_space" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="space_id">
+    Cobot ID:
+    <input type="text" name="cobot_id">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Add Space" name="submit">
+</form>
+
+</body>
+</html>
