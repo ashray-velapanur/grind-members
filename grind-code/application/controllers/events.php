102612b0bc8c92ddd629777c03c15be02852f8d2
Ashray Velapanur <ashray@Ashrays-MacBook-Pro.local>
2015-12-07 16:25:14 -0800

    saving event name in db

--- grind-code/application/controllers/events.php
+++ grind-code/application/controllers/events.php
@@ -6,12 +6,25 @@ class Events extends CI_Controller {
 
 	public function save_event(){
 		$event_id = $_GET['event_id'];
-		$sql = "INSERT INTO events (event_id) VALUES ('$event_id')";
+		$name = $_GET['name'];
+		$sql = "INSERT INTO events (event_id, name) VALUES ('$event_id', '$name')";
 		if ($this->db->query($sql) === TRUE) {
 			error_log('done!');
 		} else {
 			error_log('nope...');
 		}
 	}
+
+	public function get_event(){
+		$event_id = $_GET['id'];
+		$sql = "SELECT * FROM events WHERE id=('$event_id')";
+		if ($this->db->query($sql) === TRUE) {
+			error_log('done!');
+		} else {
+			error_log('nope...');
+		}
+	}
+
+
 }
 ?>
