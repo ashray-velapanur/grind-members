27ea16c04af3cbd69e866af0dce7156e14405015
Will Noll <will.n@hirepirates.com>
2016-01-11 15:21:08 +0530

    Fixing uploading an image for cobot spaces

--- grind-code/application/controllers/admin/locationmanagement.php
+++ grind-code/application/controllers/admin/locationmanagement.php
@@ -190,19 +190,23 @@ class LocationManagement extends CI_Controller {
 
 	public function add_update_space() {
 		error_log("In add_update_space");
-		$target_dir = "uploads/";
-		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-		$uploadOk = 1;
-		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-		// Check if image file is a actual image or fake image
 		if(isset($_POST["submit"])) {
-			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-			if($check !== false) {
-				error_log("File is an image - " . $check["mime"] . ".");
-				$uploadOk = 1;
-			} else {
-				error_log("File is not an image.");
-				$uploadOk = 0;
+			$cobot_id = $_POST["cobot_id"];
+			$tmpName = $_FILES['fileToUpload']['tmp_name'];
+			$fp = fopen($tmpName, 'r');
+			$data = fread($fp, filesize($tmpName));
+			$data = addslashes($data);
+			fclose($fp);
+			$sql = "INSERT INTO cobot_spaces";
+			$sql .= "(id, image) VALUES ('$cobot_id', '$data')";
+			try {
+				if ($this->db->query($sql) === TRUE) {
+					echo "Record created/updated successfully";
+				} else {
+					echo "Error: " . $sql . "<br>" . $this->db->error;
+				}
+			} catch (Exception $e) {
+			    error_log('Caught exception: ',  $e->getMessage(), "\n");
 			}
 		}
 	}
