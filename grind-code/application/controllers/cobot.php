6bc59a98446e6e59d5dde68b8a9860f0753ae240
Ashray Velapanur <ashray@Ashrays-MacBook-Pro.local>
2016-01-14 12:09:28 +0530

    saving access_token in db

--- grind-code/application/controllers/cobot.php
+++ grind-code/application/controllers/cobot.php
@@ -8,7 +8,6 @@ class Cobot extends CI_Controller {
 		$app_token = '061cb2b829ece8b489e9310a474df0848adbe47024b7749a2090bf4917fe543a';
 		$url = 'https://www.cobot.me/api/users';
 		$email = $_GET['email'];
-		$data = array('access_token' => $app_token, 'email' => $email);
 
 		$data = [
 			'access_token' => $app_token,
@@ -25,8 +24,36 @@ class Cobot extends CI_Controller {
 
 		curl_close($curl);
 		$result = (array)json_decode($result);
+		$id = $result['id'];
 
-		var_dump($result);
+		$url = 'https://www.cobot.me/oauth/access_token?';
+		$data = [
+			'client_id' => '26a81206b5b2b7c9a510ca0935b0febd',
+			'client_secret' => '15a365f477efa39842a493c1ac885ebea374482240c2755d882b8d41dc293532',
+			'grant_type' => 'authorization_code',
+			'code' => $result['grant_code']
+		];
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+		$result = curl_exec($curl);
+
+		curl_close($curl);
+
+		$result = (array)json_decode($result);
+		$access_token = $result['access_token'];
+		$network = 'cobot';
+
+		$sql = "INSERT INTO third_party_user (user_id, network, access_token) VALUES ('$id', '$network', '$access_token')";
+		if ($this->db->query($sql) === TRUE) {
+			error_log('done!');
+		} else {
+			error_log('nope...');
+		}
 	}
 }
 ?>
