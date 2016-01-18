f38b023539368eb0abf128691de3624cdf5ad3a0
Ashray Velapanur <ashray@Ashrays-MacBook-Pro.local>
2016-01-18 18:23:43 +0530

    added user and job tagging endpoints

--- grind-code/application/controllers/tags.php
+++ grind-code/application/controllers/tags.php
@@ -1,6 +1,8 @@
 <?
 class Tags extends CI_Controller {
 	// create table tags (id INTEGER(10) UNSIGNED AUTO_INCREMENT primary key, name varchar(255));
+	// create table user_tags (user_id INTEGER(10) UNSIGNED primary key, tag_id INTEGER(10) UNSIGNED);
+	// create table job_tags (job_id INTEGER(10) UNSIGNED primary key, tag_id INTEGER(10) UNSIGNED);
 
 	public function save_tag(){
 		$name = $_GET['name'];
@@ -31,6 +33,26 @@ class Tags extends CI_Controller {
 		$response = array();
 		while($row = mysql_fetch_assoc($result)) {
 			array_push($response, $row);
+		}
+		var_dump(json_encode($response));
+	}
+
+	public function add(){
+		$type = $_GET['type'];
+		$entity_id = $_GET['entity_id'];
+		$tag_id = $_GET['tag_id'];
+		switch ($type) {
+			case "user":
+				$sql = "INSERT INTO user_tags (user_id, tag_id) VALUES ('$entity_id', '$tag_id')";
+				break;
+			case "job":
+				$sql = "INSERT INTO job_tags (job_id, tag_id) VALUES ('$entity_id', '$tag_id')";
+				break;				
+		}
+		if ($this->db->query($sql) === TRUE) {
+			$response = array('success'=>TRUE);
+		} else {
+			$response = array('success'=>FALSE);
 		}
 		var_dump(json_encode($response));
 	}
