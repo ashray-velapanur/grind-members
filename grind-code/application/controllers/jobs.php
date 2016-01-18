86919eab22de065dfdd5591d15605e17bbcaaec8
Ashray Velapanur <ashray@Ashrays-MacBook-Pro.local>
2016-01-18 15:13:24 +0530

    endpoint to get jobs posted by user

--- grind-code/application/controllers/jobs.php
+++ grind-code/application/controllers/jobs.php
@@ -1,14 +1,15 @@
 <?
 class Jobs extends CI_Controller {
 	// id (int), title (varchar), date posted (datetime), company (varchar), dev/design (varchar)
-	// create table jobs (id INTEGER(10) UNSIGNED AUTO_INCREMENT primary key, title varchar(255), company varchar(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, type varchar(255));
+	// create table jobs (id INTEGER(10) UNSIGNED AUTO_INCREMENT primary key, title varchar(255), company varchar(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, type varchar(255), url varchar(255), posted_by varchar(255));
 
 	public function create(){
 		$title = $_GET['title'];
 		$company = $_GET['company'];
 		$type = $_GET['type'];
 		$url = $_GET['url'];
-		$sql = "INSERT INTO jobs (title, company, type, url) VALUES ('$title', '$company', '$type', '$url')";
+		$posted_by = $_GET['posted_by'];
+		$sql = "INSERT INTO jobs (title, company, type, url, posted_by) VALUES ('$title', '$company', '$type', '$url', '$posted_by')";
 		if ($this->db->query($sql) === TRUE) {
 			$response = array('success'=>TRUE);
 		} else {
@@ -18,7 +19,8 @@ class Jobs extends CI_Controller {
 	}
 
 	public function get(){
-		$sql = "SELECT * FROM jobs";
+		$posted_by = $_GET['posted_by'];
+		$sql = "SELECT * FROM jobs WHERE posted_by='".$posted_by."'";
 		$result = mysql_query($sql);
 		$response = array();
 		while($row = mysql_fetch_assoc($result))
