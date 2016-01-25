79c2272a9df636a4cfbee70dd680fd440db96cba
Ashray <ashray.velapanur@gmail.com>
2016-01-25 16:23:59 +0530

    added jobs to search

--- grind-code/application/controllers/search.php
+++ grind-code/application/controllers/search.php
@@ -8,6 +8,8 @@ class Search extends CI_Controller {
 									(select id, name, 'company' as type from company where name like '%%%s%%')
 									union
 									(select id, name, 'event' as type from event where name like '%%%s%%')
+									union
+									(select id, title as name, 'job' as type from jobs where name title '%%%s%%')
 									", $q, $q, $q));
 		$response = array();
 		while($row = mysql_fetch_assoc($query)) {
