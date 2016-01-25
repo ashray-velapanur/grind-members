56647abba2009522fc5b5e8cc11c78f0e99f5cea
Ashray <ashray.velapanur@gmail.com>
2016-01-25 12:54:06 +0530

    implemented user, company and event search

---
+++ grind-code/application/controllers/search.php
@@ -0,0 +1,19 @@
+<?
+class Search extends CI_Controller {
+	public function search(){
+		$q = $_GET['q'];
+		$query = mysql_query(sprintf("
+									(select id, first_name as name, 'user' as type from user where first_name like '%%%s%%')
+									union
+									(select id, name, 'company' as type from company where name like '%%%s%%')
+									union
+									(select id, name, 'event' as type from event where name like '%%%s%%')
+									", $q, $q, $q));
+		$response = array();
+		while($row = mysql_fetch_assoc($query)) {
+			array_push($response, $row);
+		}
+		var_dump($response);
+	}
+}
+?>
