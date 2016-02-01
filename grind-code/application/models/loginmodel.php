d197e99426db9da3f57ec3a55c9167bf8cfd4683
Will Noll <will.n@hirepirates.com>
2016-02-01 16:52:11 +0530

    Integrating Cobot User Creation with Login

--- grind-code/application/models/loginmodel.php
+++ grind-code/application/models/loginmodel.php
@@ -52,6 +52,7 @@ class LoginModel extends CI_Model {
 		$newUserId = $this->mm->doAddMember($userdata, $membershipdata, $companydata, $phonedata, $emaildata, $billingdata, $wpdata);
 		if($newUserId) {
 			$this->add_companies($newUserId, $profile);
+			$this->create_cobot_user($profile['emailAddress']);
 		}
 		return $newUserId;
 	}
@@ -84,6 +85,53 @@ class LoginModel extends CI_Model {
 			$this->db->query($sql);
 		}
 	}
+
+	private function create_cobot_user($email){
+      $app_token = '061cb2b829ece8b489e9310a474df0848adbe47024b7749a2090bf4917fe543a';
+      $url = 'https://www.cobot.me/api/users';
+
+      $data = [
+        'access_token' => $app_token,
+        'email' => $email
+      ];
+
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_POST, 1);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+      $result = curl_exec($curl);
+
+      curl_close($curl);
+      $result = (array)json_decode($result);
+      $id = $result['id'];
+
+      $url = 'https://www.cobot.me/oauth/access_token?';
+      $data = [
+        'client_id' => '26a81206b5b2b7c9a510ca0935b0febd',
+        'client_secret' => '15a365f477efa39842a493c1ac885ebea374482240c2755d882b8d41dc293532',
+        'grant_type' => 'authorization_code',
+        'code' => $result['grant_code']
+      ];
+
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_POST, 1);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+      $result = curl_exec($curl);
+
+      curl_close($curl);
+
+      $result = (array)json_decode($result);
+      $access_token = $result['access_token'];
+      $network = 'cobot';
+
+      $this->load->model("thirdpartyusermodel","tpum",true);
+      $this->tpum->create($id, $network, $access_token);
+    }
 
 	private function add_third_party_user($userId, $profile, $access_token) {
 		$sql = "INSERT INTO third_party_user (network_id, user_id, network, access_token, profile_picture) VALUES ('$profile->id', $userId, 'linkedin', '$access_token', '$profile->pictureUrl') ON DUPLICATE KEY UPDATE access_token='".$access_token."' , profile_picture='".$profile->pictureUrl."'";
