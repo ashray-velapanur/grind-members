3e2a3c964cf6936297220a8ff159a05b042e1cf8
Ashray <ashray.velapanur@gmail.com>
2016-02-03 13:35:56 +0530

    handling invlaid params

--- grind-code/application/controllers/api.php
+++ grind-code/application/controllers/api.php
@@ -268,18 +268,23 @@ class Api extends REST_Controller
 
      function search_get(){
       $q = $this->get('q');
-      $query = mysql_query(sprintf("
-                    (select id, first_name as name, 'user' as type from user where first_name like '%%%s%%')
-                    union
-                    (select id, name, 'company' as type from company where name like '%%%s%%')
-                    union
-                    (select id, name, 'event' as type from events where name like '%%%s%%')
-                    union
-                    (select id, title as name, 'job' as type from jobs where title like '%%%s%%')
-                    ", $q, $q, $q, $q));
-      $response = array();
-      while($row = mysql_fetch_assoc($query)) {
-        array_push($response, $row);
+      if (!$q) {
+        $response = array('success'=> FALSE, 'message'=>'Invalid parameters.');
+      } else {
+        $query = mysql_query(sprintf("
+                      (select id, first_name as name, 'user' as type from user where first_name like '%%%s%%')
+                      union
+                      (select id, name, 'company' as type from company where name like '%%%s%%')
+                      union
+                      (select id, name, 'event' as type from events where name like '%%%s%%')
+                      union
+                      (select id, title as name, 'job' as type from jobs where title like '%%%s%%')
+                      ", $q, $q, $q, $q));
+        $response_data = array();
+        while($row = mysql_fetch_assoc($query)) {
+          array_push($response_data, $row);
+        }
+        $response = array('success'=>TRUE, 'data'=>$response_data);
       }
       $this->response($response, 200);
      }
