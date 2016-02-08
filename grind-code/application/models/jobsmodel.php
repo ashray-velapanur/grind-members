8e2048c6a57747895c2afe28b5c7b83786b2834b
Ashray <ashray.velapanur@gmail.com>
2016-02-08 18:21:54 +0530

    added company name, user name and formatted date to jobs endpoint

--- grind-code/application/models/jobsmodel.php
+++ grind-code/application/models/jobsmodel.php
@@ -11,14 +11,15 @@ class JobsModel extends CI_Model {
 	}
 
 	function get($type=null, $posted_by=null){
+		$base_query = "select jobs.title, jobs.company_id,  date_format(jobs.created_at, '%e-%M') created_at, jobs.type, jobs.url, jobs.posted_by, company.name as company_name, user.first_name, user.last_name from jobs left join company on jobs.company_id = company.id left join user on jobs.posted_by = user.id";
 		if ($type AND $posted_by){
-			$query = mysql_query("SELECT * FROM jobs WHERE posted_by='".$posted_by."' AND type='".$type."'");
+			$query = mysql_query($base_query." where posted_by='".$posted_by."' and type='".$type."'");
 		} elseif ($type) {
-			$query = mysql_query("SELECT * FROM jobs WHERE type='".$type."'");
+			$query = mysql_query($base_query." where type='".$type."'");
 		} elseif ($posted_by) {
-			$query = mysql_query("SELECT * FROM jobs WHERE posted_by='".$posted_by."'");
+			$query = mysql_query($base_query." where posted_by='".$posted_by."'");
 		} else {
-			$query = mysql_query("SELECT * FROM jobs");
+			$query = mysql_query($base_query);
 		}
 		$response = array();
         while($row = mysql_fetch_assoc($query)) {
