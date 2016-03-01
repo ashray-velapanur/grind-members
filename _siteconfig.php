fdc2f38adc5caab0efab310c67a932e9954a4fdc
Will N <will.n@hirepirates.com>
2016-03-01 15:52:21 +0530

    Updating _siteconfig with Percolate(Staging) creds

--- _siteconfig.php
+++ _siteconfig.php
@@ -13,37 +13,33 @@ define("PRINT_CREDIT_AMOUNT", 35.00);
 $http_host = $_SERVER['HTTP_HOST'];
 
 switch($http_host){
-	case 'local.grindspaces.com':
-		define("SITE", "LOCAL");
-	break;
-	
-	case 'percolate.grindspaces.com';
-		define("SITE", "LOCAL");
-	break;
-	
-	default:
-		define("SITE", "LOCAL");
-	break;
+        case 'local.grindspaces.com':
+                define("SITE", "LOCAL");
+        break;
+
+        case 'percolate.grindspaces.com';
+                define("SITE", "STAGING");
+        break;
+
+        default:
+                define("SITE", "PRODUCTION");
+        break;
 }
 
+        define("ROOTMEMBERPATH", "/grind-members/");
+                define("CI_PATH", "grind-members/ci");  //deprecate
+                define("CIPATH", "grind-members/ci");
 
-
-        define("ROOTMEMBERPATH", "/Library/WebServer/Documents/grind-members/");
-		define("CI_PATH", "");
-		define("CIPATH", "ci");  
-        define("MEMBERINCLUDEPATH", "/Library/WebServer/Documents/grind-members/"); 
-		define("GRINDPUBLICSITEROOT", "http://localhost/grind-members");
-		define("DB_USER","root");
-		define("DB_PASSWORD","beagles666");
-		define("DB_HOST","127.0.0.1");
-		define("DB_NAME","grind_2");	
-		// recurly
-		define('RECURLY_API_USERNAME', 'api@grindspaces.com');
-		define('RECURLY_API_PASSWORD', '52bda929c0114031af81e844d5c5d0e3');
-		define('RECURLY_SUBDOMAIN', 'grindspaces');
-		define('RECURLY_ENVIRONMENT', 'production');
-		define('RECURLY_PRIVATEKEY', '6c86e4f0eaf1425c893fb3789356956c');
-
-        
-
+        define("MEMBERINCLUDEPATH", $_SERVER["DOCUMENT_ROOT"]."/grind-members/");
+                define("GRINDPUBLICSITEROOT", "http://percolate.grindspaces.com/main/");
+                define("DB_USER","583278_percolate");
+                define("DB_PASSWORD","$0gR1nd5pac3s");
+                define("DB_HOST","localhost");
+                define("DB_NAME","test");
+                // recurly
+                define('RECURLY_API_USERNAME', 'api-test@grindspaces.com');
+                define('RECURLY_API_PASSWORD', '79f928bcf89842f78d0d48771944f894');
+                define('RECURLY_SUBDOMAIN', 'grindspaces-test');
+                define('RECURLY_ENVIRONMENT', 'sandbox');
+                define('RECURLY_PRIVATEKEY', '077dc41b75684b878ea1c22aad342003');
 ?>
