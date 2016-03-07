041efe226c5096fb8560d887a610923f42cd943c
Ashray <ashray.velapanur@gmail.com>
2016-03-07 16:20:58 +0530

    testing eventbrite webhooks

--- grind-code/application/controllers/eventbrite.php
+++ grind-code/application/controllers/eventbrite.php
@@ -5,8 +5,21 @@ class Eventbrite extends CI_Controller {
 	public function callback(){
 		error_log('... eventbrite callback');
 		$request_body = file_get_contents('php://input');
-		error_log($request_body);
 		$data = json_decode($request_body);
+		$url = $data->api_url;
+	  	$data = array(
+			"token"=>"EYFPEMS6IJLSNOXNVH56"
+	  		);
+	    $options = array(
+	        'http' => array(
+	            'method'  => 'GET',
+	            'content' => http_build_query($data),
+	        ),
+	    );
+	    $context  = stream_context_create($options);
+	    $result = file_get_contents($url, false, $context);
+	    error_log($result);
+	    return json_decode($result);
 	}
 }
 
