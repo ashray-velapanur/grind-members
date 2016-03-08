7c74ec1e374ad924e8b934db92645e41b969cd49
Will N <will.n@hirepirates.com>
2016-03-08 17:39:35 +0530

    Adding endpoint to get all users for a given tag

--- grind-code/application/models/usertagsmodel.php
+++ grind-code/application/models/usertagsmodel.php
@@ -41,5 +41,23 @@ class UserTagsModel extends CI_Model {
         }
         return $response_data;
 	}
+
+	function get_users($tag_id) {
+        $response_data = array();
+        $sql = "select ".
+        			"user.id as id, concat_ws(' ', user.first_name , user.last_name) as name, ".
+        			"third_party_user.profile_picture as profile_picture, positions.designation as designation, company.name as company_name ".
+        		"from ".
+        			"user ".
+        			"left outer join user_tags on user_tags.user_id = user.id ".
+        			"left outer join third_party_user on third_party_user.user_id = user.id ".
+        			"left outer join positions on positions.company_id = user.company_id and positions.user_id = user.id ".
+        			"left outer join company on user.company_id = company.id ".
+        		"where user_tags.tag_id = ".$tag_id;
+        error_log($sql);
+        $query = $this->db->query($sql);
+        $response_data = $query->result();
+        return $response_data;
+	}
 };
 ?>
