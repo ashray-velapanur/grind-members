ddf27c5155723541ecc5292b8c50ed746cf5a302
Will N <will.n@hirepirates.com>
2016-03-10 15:24:55 +0530

    Removing deprecated code from grind code

--- grind-code/application/controllers/members/companies.php
+++ grind-code/application/controllers/members/companies.php
@@ -30,41 +30,4 @@ class Companies extends CI_Controller {
 		
 		$this->load->view('members/companies/list.php',$data);
 	}
-
-	public function all() {
-		$this->load->model('members/companymodel','',true);
-		$companies = $this->companymodel->get_all();
-		$companies_data = [];
-		foreach ($companies as $company) {
-			$company = (array)$company;
-			$company_data = [
-				'name' => $company['name'],
-				'logo' => 'data:image/jpeg;base64,'.base64_encode( $company['logo'] ),
-				'description' => $company['description']
-			];
-			array_push($companies_data, $company_data);
-		}
-		$data = [
-			"companies" => $companies_data
-		];
-		return json_encode($data);
-		//$this->load->view('members/companies/all.php',$data);
-	}
-
-	public function get_members() {
-		$id = $_GET['id'];
-		$this->load->model('members/companymodel','',true);
-		$data =  $this->companymodel->get_members($id);
-		return $data;
-		//$this->load->view('members/companies/members.php',$data);
-	}
-
-	public function get_jobs() {
-		$id = $_GET['id'];
-		$this->load->model('members/companymodel','',true);
-		$data =  $this->companymodel->get_jobs($id);
-		return $data;
-		//$this->load->view('members/companies/jobs.php',$data);
-	}
-	
 }
