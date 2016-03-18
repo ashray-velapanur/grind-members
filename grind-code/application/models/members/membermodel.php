41ab29c2c2a5d8498b9d1a1facb3fc504a6b49f7
Ranju <ranju@b-eagles.com>
2016-03-18 18:49:18 -0400

    WIP - New logic for putting jobs

--- grind-code/application/models/members/membermodel.php
+++ grind-code/application/models/members/membermodel.php
@@ -141,12 +141,10 @@ class MemberModel extends CI_Model {
     }
 
     function get_company_id($user_id){
-        /*
         $sql = "select company_id from user where id = ".$user_id;
         $query = $this->db->query($sql);
         $company_id = $query->result();
-        */
-        return "$company_id";
+        return $company_id;
     }
     
      /**
