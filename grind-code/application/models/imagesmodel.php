bb96f7f2d078ee5feaf4a556d5249ec9112cf485
Will Noll <will.n@hirepirates.com>
2016-03-28 17:22:34 +0530

    Adding method to delete images

--- grind-code/application/models/imagesmodel.php
+++ grind-code/application/models/imagesmodel.php
@@ -10,23 +10,37 @@ class ImagesModel extends CI_Model {
 		$imgName = $image['tmp_name'];
 		$imgType = $image['type'];
 		$fp = fopen($imgName, 'r');
-		$content = fread($fp, filesize($imgName));
-		$content = addslashes($content);
-		fclose($fp);
-		
-		$data = array(
-			'content' => $content,
-			'type' => $imgType
-		);
-		$sql = "INSERT INTO image (content, type) VALUES('$content', '$imgType')";
-		error_log($sql);
-		if ($this->db->query($sql) === TRUE) {
-			$image_id = $this->db->insert_id();
-			error_log("Image added");
-		} else {
-			error_log("Error: " . $this->db->error);
+		if($fp) {
+			$content = fread($fp, filesize($imgName));
+			$content = addslashes($content);
+			fclose($fp);
+			
+			$data = array(
+				'content' => $content,
+				'type' => $imgType
+			);
+			$sql = "INSERT INTO image (content, type) VALUES('$content', '$imgType')";
+			error_log($sql);
+			if ($this->db->query($sql) === TRUE) {
+				$image_id = $this->db->insert_id();
+				error_log("Image added");
+			} else {
+				error_log("Error: " . $this->db->error);
+			}
 		}
 		return $image_id;
+	}
+
+	function delete_image($image_id) {
+		if($image_id) {
+			$sql = "DELETE FROM image WHERE id='".$image_id."'";
+			error_log($sql);
+			if ($this->db->query($sql) === TRUE) {
+				error_log("Deleted image successfully");
+			} else {
+				error_log("Error: " . $sql . "<br>" . $this->db->error);
+			}
+		}
 	}
 };
 ?>
