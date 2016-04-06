0df843ec4cf0413527ec7cdd2607367b19ba0e03
Ashray <ashray.velapanur@gmail.com>
2016-04-06 13:54:20 +0530

    sorting jobs

--- grind-code/application/models/jobsmodel.php
+++ grind-code/application/models/jobsmodel.php
@@ -13,19 +13,21 @@ class JobsModel extends CI_Model {
 	function get($type=null, $posted_by=null, $company_id=null, $id=null){
 		$base_query = "select jobs.title, jobs.company_id,  date_format(jobs.created_at, '%e-%M') created_at, jobs.type, jobs.url, jobs.posted_by, company.name as company_name, user.first_name, user.last_name from jobs left join company on jobs.company_id = company.id left join user on jobs.posted_by = user.id";
 		if ($company_id) {
-			$query = mysql_query($base_query." where jobs.company_id='".$company_id."'");
+			$query = $base_query." where jobs.company_id='".$company_id."'";
 		} elseif ($type) {
-			$query = mysql_query($base_query." where jobs.type='".$type."'");
+			$query = $base_query." where jobs.type='".$type."'";
 		} elseif ($posted_by) {
-			$query = mysql_query($base_query." where jobs.posted_by='".$posted_by."'");
+			$query = $base_query." where jobs.posted_by='".$posted_by."'";
 		} elseif ($id) {
-			$query = mysql_query($base_query." where jobs.id='".$id."'");
+			$query = $base_query." where jobs.id='".$id."'";
 		} else {
-			$query = mysql_query($base_query);
+			$query = $base_query;
 		}
+		$query = $query." order by created_at desc";
 		error_log(json_encode($query));
 		$response = array();
-        while($row = mysql_fetch_assoc($query)) {
+		$mysql_query = mysql_query($query);
+        while($row = mysql_fetch_assoc($mysql_query)) {
 	      	array_push($response, $row);
         }
         return $response;
