592f1134bfacc2ebd8cf656dcbe784a7185e3834
Ashray <ashray.velapanur@gmail.com>
2016-04-07 13:28:20 +0530

    added logs to cron job

--- grind-code/application/controllers/cron.php
+++ grind-code/application/controllers/cron.php
@@ -7,6 +7,7 @@ class Cron extends CI_Controller {
     }
 
     public function reset_checkins() {
+        error_log('cron job: resetting checkin count');
         $sql = "update cobot_spaces set checkins=0";
         $this->db->query($sql);
     }
