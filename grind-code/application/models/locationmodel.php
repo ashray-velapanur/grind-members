37b185b5af8dcd726d338a561f4aa08e50d3a1ea
Ranju <ranju@b-eagles.com>
2016-07-06 15:25:27 +0530

    Using util method to post data to make Cobot booking

--- grind-code/application/models/locationmodel.php
+++ grind-code/application/models/locationmodel.php
@@ -710,21 +710,8 @@ class LocationModel extends CI_Model {
 	  	$this->load->model("thirdpartyusermodel","tpum",true);
 	  	$cobot_access_token = $this->tpum->get_cobot_access_token($user_id);
 	    if($cobot_access_token) {
-	    	$options = array(
-		        'http' => array(
-		            'header'  => "Authorization: Bearer ".$cobot_access_token."\r\n",
-		            'method'  => 'POST',
-		            'content' => http_build_query($data),
-		            'ignore_errors' => true
-		        ),
-		    );
-		    error_log('... creating booking');
-		    error_log(json_encode($options));
-		    error_log(json_encode($data));
-		    $context  = stream_context_create($options);
-		    $result = file_get_contents($url, false, $context);
-		    error_log($result);
-		    $response = json_decode($result);
+		    $util = new utilities;
+            $response = $util->do_post($url, $params=$data, $cobot_access_token);
 	    } else {
 	    	$response['errors'] = "Could not find Cobot Access Token for the user";
 	    }
